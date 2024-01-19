cb0a639cceb3d26e5577204d5fbb6b9fe7450c11
Minh <buiquangminh731@gmail.com>
2024-01-19 22:00:22 +0700

    commit

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,14 +1,16 @@
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 100f;
+    public float jumpForce = 10f;
+    public bool isGrounded = false;
+    public bool ck;
 
     private Rigidbody2D rb;
-    private bool isGrounded;
     public Transform groundCheck;
-    public LayerMask groundLayer;
 
     private void Start()
     {
@@ -16,22 +18,33 @@ public class Movement : MonoBehaviour
     }
 
     private void Update()
-    {
+    {   
         // Player movement
-        float horizontalInput = UnityEngine.Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-        rb.velocity = movement;
-
+         float horizontalInput = UnityEngine.Input.GetAxis("Horizontal");
+        ck = IsWall();
+         if(!IsWall()){
+         Vector2 movement = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+         rb.velocity = movement;
+         }
         // Player jumping
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow)&& isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    private void FixedUpdate()
-    {
-        // Check if the player is on the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    // Ground check
+    private bool IsWall(){
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, LayerMask.GetMask("Ground"));
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
+    }  
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
+        }
     }
 }
